31ac8c6741e1cdc5146ee7657c1c8fdcfa1e6ec8
yufengkz <65370392@qq.com>
2023-01-16 18:05:29 +0800

    init

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,17 +1,11 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    const name = process.env.BLOG_NAME ? decodeURI(process.env.BLOG_NAME) : '语风的博客'
+    const blogTitle = process.env.BLOG_TITLE ? decodeURI(process.env.BLOG_TITLE) : '记事'
+    const footerText = process.env.BLOG_FOOTER_TEXT ? decodeURI(process.env.BLOG_FOOTER_TEXT) : '开源'
 
-  return {
-    name,
-    blogTitle,
-    footerText,
-  };
-};
+    return {
+        name,
+        blogTitle,
+        footerText
+    }
+}
